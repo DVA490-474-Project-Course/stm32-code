ddb182c9f54729d5c2f3ba7efe20453a752bfa27
Emil Åberg <emilgamma@gmail.com>
2024-11-23 14:20:48 +0100

    Add liscence for adafruit's driver, add some comments to prebuild script

--- pre_build_script.sh
+++ pre_build_script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Pre build script that connects generated C code with
-# user written C++ code.
+# user written C++ code. Project is configured to run
+# this script automatically before each build.
 
 FILEPATH=Core/Src/main.c
 INCLUDE='#include "user-code\/main_cpp.h"'
@@ -14,7 +15,7 @@ else
   sed -i "/USER CODE BEGIN WHILE/s/$/\n  ${MAINCALL}/" ${FILEPATH}
 fi
 
-# Add include of MainCpp.h in main.c if it's missing
+# Add include of main_cpp.h in main.c if it's missing
 if grep -q "${INCLUDE}" ${FILEPATH}
 then
   :
